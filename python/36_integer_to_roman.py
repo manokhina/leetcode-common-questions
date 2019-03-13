83e545be35c843dbf944fabe182bd88d2f7916b6
Anastasia Manokhina <a.manokhina@fasten.com>
2019-03-13 20:28:20 +0300

    Add 36 and 37

--- python/36_integer_to_roman.py
+++ python/36_integer_to_roman.py
@@ -8,5 +8,24 @@ What is the range of the numbers?
 
 
 class Solution:
+    def __init__(self):
+        self.values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        self.symbols =["M", "CM", "D", "CD", "C", "XC", "L",  "XL", "X",
+                       "IX", "V", "IV", "I"]
+
     def intToRoman(self, num: 'int') -> 'str':
-        pass
+        i = 0
+        roman = []
+        while num > 0:
+            k = int(num / self.values[i])
+            for j in range(k):
+                roman.append(self.symbols[i])
+                num -= self.values[i]
+            i += 1
+        return "".join(roman)
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.intToRoman(523))
+    print(sol.intToRoman(4500))
--- python/37_roman_to_integer.py
+++ python/37_roman_to_integer.py
@@ -6,5 +6,24 @@ Input is guaranteed to be within the range from 1 to 3999.
 
 
 class Solution:
+    def __init__(self):
+        self.symbol_map = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100,
+                           'D': 500, 'M': 1000}
+
     def romanToInt(self, s: 'str') -> 'int':
-        pass
+        prev = 0
+        total = 0
+        for c in s:
+            curr = self.symbol_map[c]
+            if curr > prev:
+                total += curr - 2 * prev
+            else:
+                total += curr
+            prev = curr
+        return total
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.romanToInt("DDCXI"))
+    print(sol.romanToInt("CMIX"))
