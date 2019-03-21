c61ad84bc57c868967955b994ec1a21a90dd0936
Anastasia Manokhina <a.manokhina@fasten.com>
2019-03-21 09:11:42 +0300

    Add 47

--- python/41_valid_parentheses.py
+++ python/41_valid_parentheses.py
@@ -10,5 +10,43 @@ Q: Is the empty string valid? A: Yes.
 
 
 class Solution:
-    def isValid(self, s: 'str') -> 'bool':
-        pass
+
+    class Bracket:
+
+        def __init__(self, bracket_type):
+            self.bracket_type = bracket_type
+
+        def match(self, char):
+            if self.bracket_type == '[' and char == ']':
+                return True
+            if self.bracket_type == '{' and char == '}':
+                return True
+            if self.bracket_type == '(' and char == ')':
+                return True
+            return False
+
+    def isValid(self, sequence: 'str') -> 'bool':
+        stack = []
+        for s in sequence:
+            if s in ("[", "(", "{"):
+                stack.append(Solution.Bracket(s))
+
+            elif s in ("]", ")", "}"):
+                if not stack:
+                    return False
+
+                top = stack.pop()
+
+                if not top.match(s):
+                    return False
+
+        if not stack:
+            return True
+        return False
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    for s in ["()()((()))", "[(){{}[]}]", "[(]}}", "[", ""]:
+        answer = sol.isValid(s)
+        print(f"{s} is {answer}")
